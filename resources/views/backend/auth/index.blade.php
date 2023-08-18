bea8d90c2e6c3b3f1f995fadb50dd788497138f5
Ar Wahyu Pradana <wahyupradana@riau.go.id>
2023-08-18 18:41:22 +0700

    update login

--- resources/views/backend/auth/index.blade.php
+++ resources/views/backend/auth/index.blade.php
@@ -9,9 +9,8 @@
     <meta name="csrf-token" content="{!! csrf_token() !!}">
     <title>@stack('title',config('master.app.profile.name'))</title>
     <link rel="icon" href="{{ url(config('master.app.profile.template').config('master.app.profile.favicon'))}}">
-    <link rel="stylesheet" href="{{ url(config('master.app.web.template').'/css/vendors_css.css') }}">
+    <link rel="stylesheet" href="{{ url(config('master.app.web.template').'/assets/vendor_components/bootstrap/dist/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ url(config('master.app.web.template').'/css/style.css') }}">
-    <link rel="stylesheet" href="{{ url(config('master.app.web.template').'/css/skin_color.css') }}">
 </head>
 <body class="hold-transition theme-primary bg-img" style="background-image: url({{ url(config('master.app.web.template').config('master.app.web.background'))}})">
 @yield('content')
