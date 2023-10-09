387bb3c0dd77c5e05979f64ab6a70d4b84e2e28a
Ar Wahyu Pradana <wahyupradana@riau.go.id>
2023-10-09 11:24:31 +0700

    fix bugs on load menu after deleted

--- resources/views/backend/menu/delete.blade.php
+++ resources/views/backend/menu/delete.blade.php
@@ -37,7 +37,7 @@
         <span class="message"></span>
     </div>
 </div>
-{!! html()->hidden('function')->value('loadMenu(),sidebarMenu()')->id('function') !!}
+{!! html()->hidden('function')->value('loadMenu,sidebarMenu')->id('function') !!}
 {!! html()->form()->close() !!}
 <script>
     $('.modal-title').html('<i class="mdi mdi-delete-forever"></i> Hapus Data {!! $page->title !!}');
